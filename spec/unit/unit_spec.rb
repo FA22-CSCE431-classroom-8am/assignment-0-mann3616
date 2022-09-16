51e7797824f13f2d7e894f542e1ce42c92308639
root <root@localhost>
2022-09-16 00:56:47 +0000

    All tests implemented

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'jk rowling', publish_date: '08/09/2001', price: '10')
   end
 
   it 'is valid with valid attributes' do
@@ -10,6 +10,18 @@ RSpec.describe Book, type: :model do
   end
 
   it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without a author' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without a publish_date' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without a price' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
